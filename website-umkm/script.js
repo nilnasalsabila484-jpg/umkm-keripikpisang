461ac45a79dc8495759ac700b06e3d19b8262946
nilnasalsabila484 <nilnasalsabila484@gmail.com>
2025-12-16 13:32:15 +0700

    inisialisasi projek

---
+++ website-umkm/script.js
@@ -0,0 +1,12 @@
+alert('Selamat datang di Website Profil UMKM Keripik Pisang Nusantara!');
+
+// Menampilkan informasi variabel di console
+const namaUMKM = 'Keripik Pisang Nusantara';
+console.log('Nama UMKM:', namaUMKM);
+
+// Demonstrasi variabel dan operator
+let produk = 3;
+console.log('Jumlah varian produk saat ini:', produk);
+
+produk += 2;
+console.log('Setelah penambahan varian:', produk);
